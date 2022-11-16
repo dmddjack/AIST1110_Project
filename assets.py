3a4d9f2da6b761c67fa10dfa6eb62f94cc5959af
Anson Lam <100675992+yflam1@users.noreply.github.com>
2022-11-16 18:50:25 +0800

    Update assets.py

--- assets.py
+++ assets.py
@@ -6,7 +6,7 @@ import pygame
 class _Tank(pygame.sprite.Sprite):
     def __init__(self, window_width: int, window_height: int, image_path: str, speed: int, resize_ratio: int = 1, seed: None | int = None):
         super().__init__()
-        # seed the random module
+        # Seed the random module
         random.seed(seed)
 
         self.window_width, self.window_height = window_width, window_height
@@ -19,7 +19,7 @@ class _Tank(pygame.sprite.Sprite):
         self.angle = random.choice((0, 90, 180, 270))
         self.surf = pygame.transform.rotate(self.surf, self.angle)
 
-    def _keep_inside(self):
+    def _keep_inside(self) -> None:
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > self.window_width:
@@ -60,25 +60,21 @@ class Player(_Tank):
 
 class Enemy(_Tank):
     image_path = "images/tank_02/tank_02_A.png"
-    resize_ratio = 4.8
+    resize_ratio = 4.4
 
     def __init__(self, window_width: int, window_height: int, speed: int, seed: None | int = None) -> None:
         super().__init__(window_width, window_height, self.image_path, speed, self.resize_ratio, seed)
         if self.angle == 0:
             self.start_x = random.randint(0, self.window_width)
             self.start_y = self.window_height
-
         elif self.angle == 90:
             self.start_x = self.window_width
             self.start_y = random.randint(0, self.window_height)
-
         elif self.angle == 180:
             self.start_x = random.randint(0, self.window_width)
             self.start_y = 0
-
         else:
             self.start_x = 0
             self.start_y = random.randint(0, self.window_height)
-
         self.rect = self.surf.get_rect(center=(self.start_x, self.start_y))
         self._keep_inside()
