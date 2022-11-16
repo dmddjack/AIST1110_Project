2660fd3fa1c248555a1c66ba2fbad728772b7fb1
Anson Lam <100675992+yflam1@users.noreply.github.com>
2022-11-16 18:50:04 +0800

    Update tank_war_play.py

--- tank_war_play.py
+++ tank_war_play.py
@@ -1,53 +1,41 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-from tank_war import Tank
+
 import argparse
+
+import pygame
+
+from tank_war import TankWar
 
 
 def pressed_to_action(pressed_keys):
     action = 9
-    if pressed_keys[pygame.K_UP]:
-        action = 0
-    if pressed_keys[pygame.K_DOWN]:
-        action = 1
-    if pressed_keys[pygame.K_LEFT]:
-        action = 2
-    if pressed_keys[pygame.K_RIGHT]:
-        action = 3
-    if pressed_keys[pygame.K_SPACE]:
-        action = 4
-    if pressed_keys[pygame.K_UP] and pressed_keys[pygame.K_SPACE]:
-        action = 5
-    if pressed_keys[pygame.K_DOWN] and pressed_keys[pygame.K_SPACE]:
-        action = 6
-    if pressed_keys[pygame.K_LEFT] and pressed_keys[pygame.K_SPACE]:
-        action = 7
-    if pressed_keys[pygame.K_RIGHT] and pressed_keys[pygame.K_SPACE]:
-        action = 8
-    # if pressed_keys[pygame.K_UP] and pressed_keys[pygame.K_LEFT]:
-    #     action = 9
-    # if pressed_keys[pygame.K_UP] and pressed_keys[pygame.K_RIGHT]:
-    #     action = 10
-    # if pressed_keys[pygame.K_DOWN] and pressed_keys[pygame.K_LEFT]:
-    #     action = 11
-    # if pressed_keys[pygame.K_DOWN] and pressed_keys[pygame.K_RIGHT]:
-    #     action = 12
-    # if pressed_keys[pygame.K_UP] and pressed_keys[pygame.K_LEFT] and pressed_keys[pygame.K_SPACE]:
-    #     action = 13
-    # if pressed_keys[pygame.K_UP] and pressed_keys[pygame.K_RIGHT] and pressed_keys[pygame.K_SPACE]:
-    #     action = 14
-    # if pressed_keys[pygame.K_DOWN] and pressed_keys[pygame.K_LEFT] and pressed_keys[pygame.K_SPACE]:
-    #     action = 15
-    # if pressed_keys[pygame.K_DOWN] and pressed_keys[pygame.K_RIGHT] and pressed_keys[pygame.K_SPACE]:
-    #     action = 16
     if pressed_keys[pygame.K_q] or pressed_keys[pygame.K_ESCAPE]:
         action = -1
+    if not pressed_keys[pygame.K_SPACE]:
+        if pressed_keys[pygame.K_UP] or pressed_keys[pygame.K_w]:
+            action = 0
+        if pressed_keys[pygame.K_DOWN] or pressed_keys[pygame.K_s]:
+            action = 1
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+            action = 2
+        if pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+            action = 3
+    else:
+        action = 4
+        if pressed_keys[pygame.K_UP] or pressed_keys[pygame.K_w]:
+            action = 5
+        if pressed_keys[pygame.K_DOWN] or pressed_keys[pygame.K_s]:
+            action = 6
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+            action = 7
+        if pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+            action = 8
     return action
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--mode", type=str, required=True)
+    parser.add_argument("-m", "--mode", type=str, required=True, help="\"human\", \"read_only\" or \"rgb_array\"")
     parser.add_argument("-s", "--seed", type=int, default=None)
     parser.add_argument("-e", "--episodes", type=int, required=True)
     parser.add_argument("-ms", "--max_steps", type=int, required=True)
@@ -60,16 +48,13 @@ if __name__ == "__main__":
     except ValueError:
         print("Invalid argument(s).")
 
-    
-    env = Tank(render_mode=render_mode)
+    env = TankWar(render_mode, seed)
     for episode in range(1, episodes + 1):
-        env.reset(seed)
+        env.reset()
         if render_mode == "human":
             running = True
             while running:
-                # env.clock.tick(env.metadata["render_fps"])
                 for event in pygame.event.get():
-                    # print(event)
                     if event.type == pygame.QUIT:
                         running = False
                 pressed_keys = pygame.key.get_pressed()
