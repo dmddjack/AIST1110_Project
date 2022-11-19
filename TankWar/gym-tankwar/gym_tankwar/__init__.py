c2df76bc88adf94b6d0ca3809f2a2d1d81a5f6e9
Anson Lam <100675992+yflam1@users.noreply.github.com>
2022-11-20 01:19:13 +0800

    Delete TankWar directory

--- TankWar/gym-tankwar/gym_tankwar/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id="gym_tankwar/TankWar-v0",
-    entry_point="gym_tankwar.envs:TankWar",
-)
--- TankWar/gym-tankwar/gym_tankwar/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-# from gym_tankwar.envs.assets import Enemy, Heart, Player
-from gym_tankwar.envs.tank_war import TankWar
