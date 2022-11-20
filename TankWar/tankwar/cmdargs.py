2bc7705003ed4691f02562bf1203db1d215c716b
Anson Lam <100675992+yflam1@users.noreply.github.com>
2022-11-20 14:54:33 +0800

    Update cmdargs.py
    Set the allowed starting HP to [1-10] and limit the allowed FPS to [1-60]

--- TankWar/tankwar/cmdargs.py
+++ TankWar/tankwar/cmdargs.py
@@ -6,7 +6,7 @@ parser.add_argument("-m", "--mode", type=str, help="The render mode",
                     default="rgb_array")
 parser.add_argument("-sh", "--starting_hp", type=int,
                     help="The starting HP of the player",
-                    choices=range(1, 6), metavar="[1-5]",
+                    choices=range(1, 11), metavar="[1-10]",
                     default=3)
 parser.add_argument("-s", "--seed", type=int, 
                     help="The seed for random number generator",
@@ -18,7 +18,8 @@ parser.add_argument("-ms", "--max_steps", type=int,
                     help="The maximum number of steps in an episode", 
                     default=5400)
 parser.add_argument("-fps", "--fps", type=int, 
-                    help="The rendering speed in frames per second",
+                    help="The rendering speed in frames per second", 
+                    choices=range(1, 61), metavar="[1-60]", 
                     default=None)
 parser.add_argument("-f", "--file", type=str, 
                     help="The file name of the Q-table file",
