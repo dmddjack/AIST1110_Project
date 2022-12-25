dbd11cb7f6628931c54a9450cf1a4e15cd2fd63f
Anson Lam <yflam1@cse.cuhk.edu.hk>
2022-12-25 17:42:58 +0800

    reformat tankwar_train.py

--- TankWar/tankwar/tankwar_train.py
+++ TankWar/tankwar/tankwar_train.py
@@ -21,10 +21,11 @@ import numpy as np
 import pygame
 import tensorflow as tf
 from tensorflow import keras
+# tf.debugging.set_log_device_placement(True)
+
+from cmdargs import args
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-# tf.debugging.set_log_device_placement(True)
-from cmdargs import args
 
 
 class RLModel:
